a39a654cf67af89b658d701eb955875d4dd6bb2c
Ann-T-George <65296822+Ann-T-George@users.noreply.github.com>
2021-06-06 16:43:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Note() {
+  return <div className="note"><h1>Javascript and React.js</h1>
+  <p>This was an amazing workshop!This was an amazing workshop!This was an amazing workshop!This was an amazing workshop!This was an amazing workshop!This was an amazing workshop!This was an amazing workshop!</p></div>;
+}
+
+export default Note;
